cc66cbc10b5dabb4b2bcdf5332484ce5bd2a3209
SS56Bo <sohamsarkar345@gmail.com>
2025-11-05 01:03:49 +0530

    tRPC support

---
+++ trpc/routers/_app.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { router, procedure } from "../trpc"
+
+export const appRouter = router({
+    hello: procedure.input(
+        z.object(
+            {
+                text: z.string(),
+            }
+        )
+    ).query((opts)=>{
+        return {
+            greeting: `hello ${opts.input.text}`
+        };
+    }),
+});
+
+export type AppRouter = typeof appRouter;
